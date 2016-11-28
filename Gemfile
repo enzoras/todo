46d050c7ccf2eafb02840b22064f32b053e3f953
Enzo Rascionato <enzoras@gmail.com>
2016-11-28 23:03:09 +0000

    added password stuff

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder',     '2.4.1'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 
+gem 'bcrypt'
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128223825) do
+ActiveRecord::Schema.define(version: 20161128225520) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "cat_name"
@@ -46,8 +46,9 @@ ActiveRecord::Schema.define(version: 20161128223825) do
     t.string   "name"
     t.string   "lname"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
